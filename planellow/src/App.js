7f08c177fedcd282af9b231d6b6ac0cc7250e48e
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2023-02-01 13:49:50 +0000

    .

--- planellow/src/App.js
+++ planellow/src/App.js
@@ -7,12 +7,12 @@ function App() {
   const[openEdit, setOpenEdit] = useState(false);
   return (
     <div>
-      <button onClick={(()=>{setOpenAdd(true)})}>Open</button>
-      <button onClick={(()=>{setOpenEdit(true)})}>Edit</button>
+      <button className='w-32 ml-4 pt-2 pb-2 rounded-md' style={{background:'#E5D3D3'}} onClick={(()=>{setOpenAdd(true)})}>Add new task</button>
+      <button className='w-14 ml-4 pt-1 pb-1 rounded-md' style={{background:'#A5EC9F'}}  onClick={(()=>{setOpenEdit(true)})}>Edit</button>
       <AddForm open={openAdd} setOpen={setOpenAdd}></AddForm>
       <EditForm open={openEdit} setOpen={setOpenEdit}></EditForm>
     </div>
   );
 }
 
-export default App;
+export default App;
--- planellow/src/components/EditForm/EditForm.jsx
+++ planellow/src/components/EditForm/EditForm.jsx
@@ -42,7 +42,7 @@ export default function EditForm(props) {
         return (
             <div id='add-container' onClick={handleOuterClick}>
                 <form onSubmit={handleSubmit}>
-                    <h1>Edit Task</h1>
+                    <h1 className='text-3xl'>Edit Task</h1>
                     <button onClick={handleClose} id="close-btn">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z" /></svg>
                     </button>
